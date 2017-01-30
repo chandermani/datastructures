50f9469874918784475ba71f8d0f6cbb60dc6a3b
chandermani <chandermani@outlook.com>
2017-01-30 15:19:57 +0000

    Add project files.

---
+++ DataStructures/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Trie<int> tries = new Trie<int>();
+        tries.Add("chander", 44);
+        tries.Add("chandermani", 55);
+        Console.WriteLine("chander {0}", 44);
+        Console.WriteLine("chandermani {0}", 55);
+        Console.WriteLine("test {0}", 0);
+
+        Console.ReadLine();
+    }
+}
---
+++ DataStructures/Trie.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Trie<T>
+{
+    public TrieNode<T> Root { get; set; }
+
+    public Trie()
+    {
+        this.Root = new TrieNode<T>('-');
+    }
+
+    public void Add(string key, T data)
+    {
+        TrieNode<T> node = this.Root;
+        for (int i = 0; i < key.Length; i++)
+        {
+            node = node.AddChild(key[i]);
+        }
+
+        node.Data = data;
+    }
+
+    public T Get(string key)
+    {
+        TrieNode<T> node = this.Root;
+
+        if (string.IsNullOrWhiteSpace(key))
+        {
+            return default(T);
+        }
+
+        for (int i = 0; i < key.Length; i++)
+        {
+            node = node.GetChild(key[i]);
+            if (node == null)
+            {
+                return default(T);
+            }
+        }
+
+        return node.Data;
+    }
+
+    public bool IsMember(string key)
+    {
+        return false;
+    }
+}
+
+public class TrieNode<T>
+{
+    private const int CHARS = 26;
+    public TrieNode(char key)
+    {
+        this.Key = key;
+        this.Children = new TrieNode<T>[CHARS];
+    }
+
+    public char Key { get; private set; }
+    public T Data { get; set; }
+    public TrieNode<T>[] Children { get; set; }
+
+    public TrieNode<T> AddChild(char key)
+    {
+        if (this.Children[this.KeyIndex(key)] == null)
+        {
+            this.Children[this.KeyIndex(key)] = new TrieNode<T>(key);
+        }
+        return Children[this.KeyIndex(key)];
+    }
+
+    public int KeyIndex(char key)
+    {
+        return key - 'a';
+    }
+
+    public TrieNode<T> GetChild(char key)
+    {
+        return this.Children[this.KeyIndex(key)];
+    }
+}
